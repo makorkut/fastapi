c9fddf68db8c5a9b675aaba531f7d54cddb8351e
makorkut <makorkut@gmail.com>
2025-10-23 13:42:25 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
 
+app = FastAPI(title="FastAPI Coolify Starter")
+
+class Echo(BaseModel):
+    message: str
+
+@app.get("/healthz")
+async def healthz():
+    return {"status": "ok"}
+
+@app.get("/")
+async def root():
+    return {"hello": "world"}
+
+@app.post("/echo")
+async def echo(payload: Echo):
+    return {"you_said": payload.message}
